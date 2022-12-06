5f9f8f6d565c6958fd0ccb85823d1cb1e34d8c58
HEESEOK JEONG <dev.heeseok@gmail.com>
2022-12-06 22:49:52 +0900

    impl: transaction 추가

--- src/API_CORE/MTransaction.cpp
+++ src/API_CORE/MTransaction.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "MTransaction.h"
+#include "MTableSpace.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -7,10 +8,33 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-MTransaction::MTransaction()
+MTransaction::MTransaction(MTableSpace* pTableSpace)
+	: m_pTableSpace(pTableSpace)
 {
 }
 
 MTransaction::~MTransaction()
 {
+	ASSERT(!m_pTableSpace->IsTR());
+	
+	if (m_pTableSpace->IsTR()) 
+		Rollback();
+}
+
+BOOL MTransaction::Begin(CString strName)
+{
+	ASSERT(!m_pTableSpace->IsTR());
+	return m_pTableSpace->BeginTR(strName);
+}
+
+BOOL MTransaction::Commit()
+{
+	ASSERT(m_pTableSpace->IsTR());
+	return m_pTableSpace->CommitTR();
+}
+
+BOOL MTransaction::Rollback()
+{
+	ASSERT(m_pTableSpace->IsTR());
+	return m_pTableSpace->RollbackTR();
 }
--- src/API_CORE/MTransaction.h
+++ src/API_CORE/MTransaction.h
@@ -1,10 +1,19 @@
 #pragma once
 
+class MTableSpace;
 class AFX_EXT_CLASS MTransaction final
 {
 public:
-	MTransaction();
+	MTransaction(MTableSpace* pTableSpace);
 	~MTransaction();
+
+public:
+	BOOL Begin(CString strName);
+	BOOL Commit();
+	BOOL Rollback();
+
+protected:
+	MTableSpace* m_pTableSpace;
 
 };
 
